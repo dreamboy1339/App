a2de13ca594be81b753c37ae67b32645c5daa8d1
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2024-11-19 22:30:01 +0900

    💄 Header 추가

---
+++ core/designsystem/src/main/java/com/hjw/designsystem/component/Header.kt
@@ -0,0 +1,81 @@
+package com.hjw.designsystem.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hjw.designsystem.AppPreview
+
+@Composable
+fun Header(
+    modifier: Modifier = Modifier,
+    title: String = "",
+    iconUrl: String = "",
+    linkUrl: String = "",
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(6.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconText(
+            modifier = Modifier.weight(1f),
+            title = title,
+            iconUrl = iconUrl,
+        )
+
+        if (linkUrl.isNotBlank()) {
+            Text(text = "전체")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HeaderWithNoParamsPreview() {
+    AppPreview {
+        Header(title = "클리어런스")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HeaderWithLinkUrlPreview() {
+    AppPreview {
+        Header(
+            title = "클리어런스",
+            linkUrl = "https://www.musinsa.com/brands/discoveryexpedition?category3DepthCodes=&category2DepthCodes=&category1DepthCode=018&colorCodes=&startPrice=&endPrice=&exclusiveYn=&includeSoldOut=&saleGoods=&timeSale=&includeKeywords=&sortCode=discount_rate&tags=&page=1&size=90&listViewType=small&campaignCode=&groupSale=&outletGoods=false&boutiqueGoods="
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HeaderWithIconUrlPreview() {
+    AppPreview {
+        Header(
+            title = "클리어런스",
+            iconUrl = "https://image.msscdn.net/icons/mobile/clock.png",
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HeaderWithAllParamsPreview() {
+    AppPreview {
+        Header(
+            title = "클리어런스",
+            iconUrl = "https://image.msscdn.net/icons/mobile/clock.png",
+            linkUrl = "https://www.musinsa.com/brands/discoveryexpedition?category3DepthCodes=&category2DepthCodes=&category1DepthCode=018&colorCodes=&startPrice=&endPrice=&exclusiveYn=&includeSoldOut=&saleGoods=&timeSale=&includeKeywords=&sortCode=discount_rate&tags=&page=1&size=90&listViewType=small&campaignCode=&groupSale=&outletGoods=false&boutiqueGoods="
+        )
+    }
+}
