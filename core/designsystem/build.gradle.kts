8bff3a8fa6e68e05bf8572229873f487bcd99384
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2024-11-19 20:40:46 +0900

    🚧 designsystem 모듈에 컴포즈 의존성 추가

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -34,9 +34,14 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+
     implementation(libs.material)
+    implementation(libs.androidx.material3)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
