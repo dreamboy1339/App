c3617ded9b7f2ed7b0e8246d6596c7ddd58bada7
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2024-11-19 22:27:33 +0900

    💄 IconText 추가

---
+++ core/designsystem/src/main/java/com/hjw/designsystem/component/IconText.kt
@@ -0,0 +1,43 @@
+package com.hjw.designsystem.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hjw.designsystem.AppPreview
+
+@Composable
+fun IconText(
+    modifier: Modifier = Modifier,
+    title: String = "",
+    iconUrl: String = "",
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = title)
+        Spacer(modifier = Modifier.size(6.dp))
+        if (iconUrl.isNotBlank()) {
+            ImageIcon(iconUrl = iconUrl)
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun IconTextPreview() {
+    AppPreview {
+        IconText(
+            title = "클리어런스",
+            iconUrl = "https://image.msscdn.net/icons/mobile/clock.png",
+        )
+    }
+}
