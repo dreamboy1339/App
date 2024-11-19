8351245389278ea514e9a55e32b2f5a2c1b365db
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2024-11-19 22:02:05 +0900

    🚧 네트워크 이미지 프리뷰 처리 가능한 AppPreview 추가

---
+++ core/designsystem/src/main/java/com/hjw/designsystem/AppPreview.kt
@@ -0,0 +1,26 @@
+@file:OptIn(ExperimentalCoilApi::class)
+
+package com.hjw.designsystem
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.graphics.toArgb
+import coil3.annotation.ExperimentalCoilApi
+import coil3.compose.AsyncImagePreviewHandler
+import coil3.compose.LocalAsyncImagePreviewHandler
+import coil3.test.FakeImage
+import com.hjw.designsystem.theme.ProductDisplayAppTheme
+import com.hjw.designsystem.theme.PurpleGrey80
+
+val previewHandler = AsyncImagePreviewHandler {
+    FakeImage(color = PurpleGrey80.toArgb())
+}
+
+@Composable
+fun AppPreview(content: @Composable () -> Unit) {
+    CompositionLocalProvider(LocalAsyncImagePreviewHandler provides previewHandler) {
+        ProductDisplayAppTheme {
+            content()
+        }
+    }
+}
