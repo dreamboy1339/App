159af61681728f69ba08039fb95d514f17a07ba7
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2024-11-19 20:24:55 +0900

    💄 Catalog 선언 추가

---
+++ app-catalog/src/main/java/com/hjw/app_catalog/Catalog.kt
@@ -0,0 +1,29 @@
+package com.hjw.app_catalog
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.hjw.app_catalog.ui.theme.ProductDisplayAppTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Catalog() {
+    ProductDisplayAppTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text("Catalog")
+                    }
+                )
+            }
+        ) { innerPadding ->
+            innerPadding
+        }
+    }
+}
