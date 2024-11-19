d6977d7f6523286ac31847e10fbaae7c054ad671
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2024-11-19 21:33:44 +0900

    🚧 ui tooling 및 coil3 추가

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -41,10 +41,13 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling)
     implementation(libs.androidx.ui.tooling.preview)
 
     implementation(libs.material)
     implementation(libs.androidx.material3)
+
+    implementation(libs.coil.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
