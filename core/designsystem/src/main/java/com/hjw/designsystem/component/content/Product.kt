2215c079c4c64fba64c6ced4518b2fa8cbf9b7ec
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2024-11-20 21:48:37 +0900

    🚧 브랜드 이름 한줄 줄임 표시 처리

--- core/designsystem/src/main/java/com/hjw/designsystem/component/content/Product.kt
+++ core/designsystem/src/main/java/com/hjw/designsystem/component/content/Product.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hjw.designsystem.AppPreview
@@ -53,6 +54,7 @@ private fun ProductInfo(
             .fillMaxWidth(),
         text = brandName,
         maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
         style = MaterialTheme.typography.labelSmall,
         color = MaterialTheme.colorScheme.tertiary
     )
@@ -83,7 +85,7 @@ private fun ProductPreview() {
         Product(
             linkUrl = "https://www.musinsa.com/app/goods/2281818",
             thumbnailUrl = "https://image.msscdn.net/images/goods_img/20211224/2281818/2281818_1_320.jpg",
-            brandName = "아스트랄 프로젝션",
+            brandName = "아스트랄 프로젝션 아스트랄 프로젝션 아스트랄 프로젝션",
             price = 39900,
             saleRate = 50,
             hasCoupon = true,
