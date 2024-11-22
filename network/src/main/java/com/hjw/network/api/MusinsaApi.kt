ca836154cbc3afe040ca5b585c532de25d4854ca
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2024-11-22 19:39:36 +0900

    🚧 Api 응답 타입 수정

--- network/src/main/java/com/hjw/network/api/MusinsaApi.kt
+++ network/src/main/java/com/hjw/network/api/MusinsaApi.kt
@@ -1,10 +1,11 @@
 package com.hjw.network.api
 
+import com.hjw.network.model.ApiResponse
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface MusinsaApi {
 
     @GET("/interview/list.json")
-    suspend fun fetchWidgetList(): Response<Any>
+    suspend fun fetchWidgetList(): Response<ApiResponse>
 }
