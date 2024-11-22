275125e1b0ee27b733fe2730f039a71c678e2c2d
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2024-11-22 19:19:06 +0900

    🚧 kotlin serialization 추가

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -34,6 +35,7 @@ android {
 
 dependencies {
 
+    implementation(libs.kotlinx.serialization.json)
     implementation(libs.bundles.retrofit)
 
     testImplementation(libs.junit)
