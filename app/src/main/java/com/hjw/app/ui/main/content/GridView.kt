2c968efdde6237f674244873cb24d0699668d888
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2024-11-24 20:11:07 +0900

    🚧 그리드 뷰에서 아이템 간격 넣기

--- app/src/main/java/com/hjw/app/ui/main/content/GridView.kt
+++ app/src/main/java/com/hjw/app/ui/main/content/GridView.kt
@@ -3,21 +3,25 @@ package com.hjw.app.ui.main.content
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.hjw.designsystem.component.content.MDSProduct
 import com.hjw.designsystem.theme.MDSColor
+import com.hjw.designsystem.theme.Spacing
 import com.hjw.domain.model.content.Goods
 
 @Composable
 fun GridView(
     goods: Goods,
+    columns: Int,
+    rows: Int,
     modifier: Modifier = Modifier,
-    columns: Int = 3,
-    rows: Int = 2,
 ) {
     Column(
         modifier = modifier
@@ -25,30 +29,64 @@ fun GridView(
             .wrapContentHeight()
     ) {
         for (row in 0 until rows) {
-            Row(
+            GridRow(
+                columns = columns,
+                row = row,
+                goods = goods
+            )
+        }
+    }
+}
+
+@Composable
+private fun GridRow(
+    columns: Int,
+    row: Int,
+    goods: Goods,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(bottom = 20.dp)
+    ) {
+        for (column in 0 until columns) {
+            val position = calculateProductPosition(
+                row = row,
+                column = column,
+                columns = columns
+            )
+
+            val isItemPadding = calculateItemPadding(
+                position = position,
+                columns = columns
+            )
+            if (isItemPadding) {
+                Spacer(modifier = Modifier.size(Spacing.xxs))
+            }
+
+            val product = goods[position]
+            MDSProduct(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-            ) {
-                for (column in 0 until columns) {
-                    val position = calculateProductPosition(row, columns, column)
-                    val product = goods[position]
-                    MDSProduct(
-                        modifier = Modifier
-                            .weight(1f)
-                            .border(width = 1.dp, color = MDSColor.Orange),
-                        linkUrl = product.linkUrl,
-                        thumbnailUrl = product.thumbnailUrl,
-                        brandName = product.brandName,
-                        price = product.price,
-                        saleRate = product.saleRate,
-                        hasCoupon = product.hasCoupon
-                    )
-                }
+                    .weight(1f)
+                    .border(width = 1.dp, color = MDSColor.Orange),
+                linkUrl = product.linkUrl,
+                thumbnailUrl = product.thumbnailUrl,
+                brandName = product.brandName,
+                price = product.price,
+                saleRate = product.saleRate,
+                hasCoupon = product.hasCoupon
+            )
+
+            if (isItemPadding) {
+                Spacer(modifier = Modifier.size(Spacing.xxs))
             }
         }
     }
 }
 
 @Composable
-private fun calculateProductPosition(row: Int, columns: Int, column: Int) = row * columns + column
+private fun calculateItemPadding(position: Int, columns: Int): Boolean = position % columns == 1
+
+@Composable
+private fun calculateProductPosition(row: Int, column: Int, columns: Int) = row * columns + column
