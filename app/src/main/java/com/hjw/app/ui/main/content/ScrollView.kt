db458944c15adc5075d8aa7741c4c2f7c61256ce
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2024-11-24 14:17:09 +0900

    🚧 ScrollView 추가 (#28)

--- app/src/main/java/com/hjw/app/ui/main/content/ScrollView.kt
+++ app/src/main/java/com/hjw/app/ui/main/content/ScrollView.kt
@@ -1,9 +1,40 @@
 package com.hjw.app.ui.main.content
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.hjw.designsystem.component.content.MDSProduct
+import com.hjw.designsystem.theme.MDSColor
 import com.hjw.domain.model.content.Goods
 
 @Composable
 fun ScrollView(goods: Goods) {
 
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = Modifier.height(500.dp)
+    ) {
+        items(count = goods.size) {
+            val product = goods[it]
+            MDSProduct(
+                modifier = Modifier
+                    .border(1.dp, color = MDSColor.Orange)
+                    .width(150.dp),
+                linkUrl = product.linkUrl,
+                thumbnailUrl = product.thumbnailUrl,
+                brandName = product.brandName,
+                price = product.price,
+                saleRate = product.saleRate,
+                hasCoupon = product.hasCoupon
+            )
+        }
+    }
 }
