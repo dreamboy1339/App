4af4485a6e3ebd0ef34e2c8520260ca2cc4a8e4a
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2024-11-24 12:06:31 +0900

    Feature/grid UI (#27)
    * 🚧 Product modifier 처리 수정
    
    * 🚧 Grid 뷰 추가

--- app/src/main/java/com/hjw/app/ui/main/content/GridView.kt
+++ app/src/main/java/com/hjw/app/ui/main/content/GridView.kt
@@ -1,9 +1,49 @@
 package com.hjw.app.ui.main.content
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.hjw.designsystem.component.content.MDSProduct
 import com.hjw.domain.model.content.Goods
 
 @Composable
-fun GridView(goods: Goods) {
-
+fun GridView(
+    goods: Goods,
+    modifier: Modifier = Modifier,
+    columns: Int = 3,
+    rows: Int = 2,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+        for (row in 0 until rows) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+            ) {
+                for (column in 0 until columns) {
+                    val position = calculateGridPosition(row, columns, column)
+                    val product = goods[position]
+                    MDSProduct(
+                        modifier = Modifier.weight(1f),
+                        linkUrl = product.linkUrl,
+                        thumbnailUrl = product.thumbnailUrl,
+                        brandName = product.brandName,
+                        price = product.price,
+                        saleRate = product.saleRate,
+                        hasCoupon = product.hasCoupon
+                    )
+                }
+            }
+        }
+    }
 }
+
+@Composable
+private fun calculateGridPosition(row: Int, columns: Int, column: Int) = row * columns + column
