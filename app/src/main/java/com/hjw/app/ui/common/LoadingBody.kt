e72816b61fa58f4861c8c4fc985e5a10ee6d56cf
dreamboy1339 <dreamboy1339@gmail.com>
2024-11-25 16:13:02 +0900

    🚧 프리뷰 추가

--- app/src/main/java/com/hjw/app/ui/common/LoadingBody.kt
+++ app/src/main/java/com/hjw/app/ui/common/LoadingBody.kt
@@ -8,7 +8,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.hjw.designsystem.AppPreview
 
 @Composable
 fun LoadingBody(
@@ -26,5 +28,13 @@ fun LoadingBody(
             color = MaterialTheme.colorScheme.secondary,
             trackColor = MaterialTheme.colorScheme.surfaceVariant,
         )
+    }
+}
+
+@Preview
+@Composable
+private fun LoadingPreview() {
+    AppPreview {
+        LoadingBody(shown = true)
     }
 }
