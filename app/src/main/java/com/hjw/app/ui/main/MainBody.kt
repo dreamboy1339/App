dc91442ac6a9112560bb6905661934596c6b77e1
dreamboy1339 <dreamboy1339@gmail.com>
2024-11-25 16:27:07 +0900

    🚧 overscroll 효과 제거

--- app/src/main/java/com/hjw/app/ui/main/MainBody.kt
+++ app/src/main/java/com/hjw/app/ui/main/MainBody.kt
@@ -1,6 +1,7 @@
 package com.hjw.app.ui.main
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.LocalOverscrollConfiguration
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,68 +23,71 @@ import com.hjw.domain.common.ContentType
 import com.hjw.domain.model.Widgets
 import com.hjw.domain.model.content.Banners
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainBody(
     widgets: Widgets,
     modifier: Modifier = Modifier,
 ) {
-    LazyColumn(
-        modifier = modifier
-            .fillMaxWidth(),
-        content = {
-            itemsIndexed(
-                items = widgets,
-                key = { index, item -> "$index-${item.hashCode()}" }
-            ) { _, widget ->
-                val contents = widget.contents
-                val contentType = contents.type
-                val contentItems = contents.contentItems
+    CompositionLocalProvider(LocalOverscrollConfiguration provides null) {
+        LazyColumn(
+            modifier = modifier
+                .fillMaxWidth(),
+            content = {
+                itemsIndexed(
+                    items = widgets,
+                    key = { index, item -> "$index-${item.hashCode()}" }
+                ) { _, widget ->
+                    val contents = widget.contents
+                    val contentType = contents.type
+                    val contentItems = contents.contentItems
 
-                val horizontalPadding = dimensionResource(R.dimen.content_horizontal_padding)
-                when (contentType) {
-                    ContentType.NONE -> {}
-                    ContentType.BANNER -> {
-                        BannerItemView(
-                            banners = contentItems as Banners
-                        )
-                    }
+                    val horizontalPadding = dimensionResource(R.dimen.content_horizontal_padding)
+                    when (contentType) {
+                        ContentType.NONE -> {}
+                        ContentType.BANNER -> {
+                            BannerItemView(
+                                banners = contentItems as Banners
+                            )
+                        }
 
-                    ContentType.GRID -> {
-                        GridItemView(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(horizontal = horizontalPadding),
-                            widget = widget,
-                            contentItems = contentItems
-                        )
-                    }
+                        ContentType.GRID -> {
+                            GridItemView(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = horizontalPadding),
+                                widget = widget,
+                                contentItems = contentItems
+                            )
+                        }
 
-                    ContentType.SCROLL -> {
-                        ScrollItemView(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(horizontal = horizontalPadding),
-                            widget = widget,
-                            contentItems = contentItems
-                        )
-                    }
+                        ContentType.SCROLL -> {
+                            ScrollItemView(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = horizontalPadding),
+                                widget = widget,
+                                contentItems = contentItems
+                            )
+                        }
 
-                    ContentType.STYLE -> {
-                        StyleItemView(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(horizontal = horizontalPadding),
-                            widget = widget,
-                            contentItems = contentItems
-                        )
+                        ContentType.STYLE -> {
+                            StyleItemView(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = horizontalPadding),
+                                widget = widget,
+                                contentItems = contentItems
+                            )
+                        }
                     }
                 }
+                item {
+                    Spacer(modifier = Modifier.size(dimensionResource(R.dimen.bottom_spacing)))
+                }
             }
-            item {
-                Spacer(modifier = Modifier.size(dimensionResource(R.dimen.bottom_spacing)))
-            }
-        }
-    )
+        )
+    }
 }
 
 @Preview
