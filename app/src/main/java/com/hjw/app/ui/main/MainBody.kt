6f67c02184ba5a909bfe32beb8c7d38525d18a3b
dreamboy1339 <dreamboy1339@gmail.com>
2024-11-25 13:50:59 +0900

    🚧 배너 리패키지

--- app/src/main/java/com/hjw/app/ui/main/MainBody.kt
+++ app/src/main/java/com/hjw/app/ui/main/MainBody.kt
@@ -10,7 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.hjw.app.ui.main.content.BannerItem
+import com.hjw.app.ui.main.content.banner.BannerItem
 import com.hjw.app.ui.main.content.grid.GridItem
 import com.hjw.app.ui.main.content.scroll.ScrollItem
 import com.hjw.app.ui.main.content.style.StyleItem
--- app/src/main/java/com/hjw/app/ui/main/content/banner/BannerItem.kt
+++ app/src/main/java/com/hjw/app/ui/main/content/banner/BannerItem.kt
@@ -1,4 +1,4 @@
-package com.hjw.app.ui.main.content
+package com.hjw.app.ui.main.content.banner
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
